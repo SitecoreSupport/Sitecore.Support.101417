bc9e70435fb7d304dee5db7fabd0e2d2d296b849
Evgen Savinov <evs@sitecore.net>
2018-03-19 16:13:21 +0200

    Fixed additional scenario with MVC Preview mode

---
+++ src/Sitecore.Support.101417/AddWebEditRibbon.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Web;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceEditor.Pipelines.GetExperienceEditorRibbon;
+using Sitecore.Mvc.ExperienceEditor.Pipelines.RenderPageExtenders;
+using Sitecore.Web.UI;
+
+namespace Sitecore.Support.Mvc.ExperienceEditor.Pipelines.RenderPageExtenders.SpeakRibbon
+{
+  public class RenderPageEditorSpeakExtender : RenderPageExtendersProcessor
+  {
+    protected override bool Render(TextWriter writer)
+    {
+      try
+      {
+        Assert.ArgumentNotNull(writer, "writer");
+        GetExperienceEditorRibbonArgs expr_10 = new GetExperienceEditorRibbonArgs();
+        ExecuteGetExperienceEditorRibbon.Run(expr_10);
+        var webControl = expr_10.Control as WebControl;
+        if (webControl == null)
+          return false;
+        writer.Write(webControl.RenderAsText());
+        return true;
+      }
+      catch (System.Web.HttpException ex)
+      { 
+
+      }
+      return false;
+    }
+
+  }
+}
---
+++ src/Sitecore.Support.101417/EnsureLoggedInForPreview.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Web;
+using Sitecore.Configuration;
+using Sitecore.Pipelines;
+using Sitecore.Shell.Web;
+using Sitecore.Sites;
+
+namespace Sitecore.Support.Mvc.Pipelines.Request.RequestBegin
+{
+
+    public class EnsureLoggedInForPreview
+    {
+      [SuppressMessage("Microsoft.Performance", "CA1822:Mark members as static", Justification = "This will introduce breaking changes.")]
+      public void Process(PipelineArgs args)
+      {
+        if (Context.PageMode.IsNormal)
+        {
+          return;
+        }
+        if (Context.IsLoggedIn)
+        {
+          return;
+        }
+        SiteContext site = Factory.GetSite("shell");
+       /* using (new SiteContextSwitcher(site))
+        {
+          ShellPage.IsLoggedIn();
+        }*/
+      }
+    }
+
+}
